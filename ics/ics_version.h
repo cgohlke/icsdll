bb4f021fe05ff8cdd16679a2c1ea6bbfe143f9c1
Christoph Gohlke <cgohlke@uci.edu>
2022-02-05 19:45:04 -0800

    Update ics/ics_version.h

--- ics/ics_version.h
+++ ics/ics_version.h
@@ -1,10 +1,10 @@
 /* ics_version.h */
 
-#define ICS_VERSION_STR "2021.3.2"
-#define ICS_FILEVERSION 2021, 3, 2, 0
+#define ICS_VERSION_STR "2022.2.6"
+#define ICS_FILEVERSION 2022, 2, 6, 0
 #define ICS_NAME "ICSx64.dll"
 #define ICS_DESCRIPTION "Image Correlation Spectroscopy Library"
 #define ICS_AUTHOR "Christoph Gohlke"
-#define ICS_COPYRIGHT "Copyright (C) 2016-2021"
+#define ICS_COPYRIGHT "Copyright (C) 2016-2022"
 #define ICS_COMPANY \
     "Laboratory for Fluorescence Dynamics, University of California, Irvine"
