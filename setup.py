93a3e5e2144c144d90469e7a50a64f1e42bf390d
Christoph Gohlke <cgohlke@cgohlke.com>
2023-01-07 00:55:56 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,71 +23,92 @@ if (
 ):
     raise RuntimeError(PACKAGE + ' requires 64-bit Python>=3.8 for Windows')
 
+
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
 with open(PACKAGE + '/icsdll.py') as fh:
     code = fh.read()
 
-version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
+version = search(r"__version__ = '(.*?)'", code)
 
-description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
 
-readme = re.search(
+readme = search(
     r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
     code,
     re.MULTILINE | re.DOTALL,
-).groups()[0]
-
+)
 readme = '\n'.join(
     [description, '=' * len(description)] + readme.splitlines()[1:]
 )
 
-license = re.search(
-    r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
-    code,
-    re.MULTILINE | re.DOTALL,
-).groups()[0]
-
-license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
-    with open('LICENSE', 'w') as fh:
-        fh.write(license)
+    # update LICENSE and README files
+
     with open('README.rst', 'w') as fh:
         fh.write(readme)
 
-for f in glob.glob(os.path.join(PACKAGE, DLLNAME + '.*')):
-    try:
-        os.remove(f)
-    except Exception:
-        pass
+    license = search(
+        r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+        code,
+        re.MULTILINE | re.DOTALL,
+    )
+    license = license.replace('# ', '').replace('#', '')
 
-sources = [
-    os.path.join('ics', c)
-    for c in ('dllmain.cpp', 'ics.cpp', 'rfft.cpp', 'yxt.cpp', 'nlsp.cpp')
-]
+    with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
 
-compiler = ccompiler.new_compiler()
-objects = compiler.compile(
-    sources,
-    'build',
-    extra_preargs=['/EHsc', '/FD', '/DICS_EXPORTS', '/openmp', '/DICS_SIMFCS'],
-    include_dirs=['ics', ONEAPI_ROOT + '/mkl/latest/include'],
-)
+else:
+    # build DLL
 
-compiler.link_shared_lib(
-    objects,
-    os.path.join(PACKAGE, DLLNAME),
-    extra_preargs=['/DLL'],
-    libraries=[
-        'mkl_core',
-        'mkl_intel_lp64',
-        'mkl_sequential',
-        # 'mkl_intel_thread',
-        # 'libiomp5md',
-    ],
-    library_dirs=[
-        ONEAPI_ROOT + '/mkl/latest/lib/intel64',
-    ],
-)
+    for f in glob.glob(os.path.join(PACKAGE, DLLNAME + '.*')):
+        try:
+            os.remove(f)
+        except Exception:
+            pass
+
+    sources = [
+        os.path.join('ics', c)
+        for c in ('dllmain.cpp', 'ics.cpp', 'rfft.cpp', 'yxt.cpp', 'nlsp.cpp')
+    ]
+
+    compiler = ccompiler.new_compiler()
+    objects = compiler.compile(
+        sources,
+        'build',
+        extra_preargs=[
+            '/EHsc',
+            '/FD',
+            '/DICS_EXPORTS',
+            '/DICS_SIMFCS',
+            '/openmp',
+        ],
+        include_dirs=['ics', ONEAPI_ROOT + '/mkl/latest/include'],
+    )
+
+    compiler.link_shared_lib(
+        objects,
+        os.path.join(PACKAGE, DLLNAME),
+        extra_preargs=['/DLL'],
+        libraries=[
+            'mkl_core',
+            'mkl_intel_lp64',
+            'mkl_sequential',
+            # 'mkl_intel_thread',
+            # 'libiomp5md',
+        ],
+        library_dirs=[
+            ONEAPI_ROOT + '/mkl/latest/lib/intel64',
+        ],
+    )
 
 setup(
     name=PACKAGE,
@@ -95,24 +116,24 @@ setup(
     description=description,
     long_description=readme,
     author='Christoph Gohlke',
-    author_email='cgohlke@uci.edu',
-    url='https://www.lfd.uci.edu/~gohlke/',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
     project_urls={
         'Bug Tracker': 'https://github.com/cgohlke/icsdll/issues',
         'Source Code': 'https://github.com/cgohlke/icsdll',
         # 'Documentation': 'https://',
     },
     python_requires='>=3.8',
-    install_requires=['numpy>=1.19.5'],
+    install_requires=['numpy'],
     setup_requires=['setuptools>=19.0'],
     packages=[PACKAGE],
-    package_data={PACKAGE: ['*.dll']},
+    package_data={PACKAGE: [f'{DLLNAME}.dll', '*.dll']},
     libraries=[('', {'sources': []})],  # sets ispurelib = False
     license='BSD',
     zip_safe=False,
     platforms=['Windows'],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
@@ -122,5 +143,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
