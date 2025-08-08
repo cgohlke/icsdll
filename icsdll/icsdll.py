82ccd21cd456698e22e5a8f0a78942e94b80bded
Christoph Gohlke <cgohlke@cgohlke.com>
2025-08-08 08:57:50 -0700

    Update icsdll/icsdll.py

--- icsdll/icsdll.py
+++ icsdll/icsdll.py
@@ -51,7 +51,7 @@ time series data:
 
 :Author: `Christoph Gohlke <https://www.cgohlke.com>`_
 :License: BSD 3-Clause
-:Version: 2025.1.6
+:Version: 2025.8.8
 
 Requirements
 ------------
@@ -59,15 +59,20 @@ Requirements
 This revision was tested with the following requirements and dependencies
 (other versions may work):
 
-- `CPython <https://www.python.org>`_ 3.10.11, 3.11.9, 3.12.8, 3.13.1 64-bit
-- `Numpy <https://pypi.org/project/numpy>`_ 2.2.1
+- `CPython <https://www.python.org>`_ 3.11.9, 3.12.10, 3.13.6 3.14.0rc 64-bit
+- `Numpy <https://pypi.org/project/numpy>`_ 2.3.2
 - `Intel(r) oneAPI Math Kernel Library <https://software.intel.com/mkl>`_
-  2025.0 (build)
+  2025.2 (build)
 - `Visual Studio 2022 C++ compiler <https://visualstudio.microsoft.com/>`_
   (build)
 
 Revisions
 ---------
+
+2025.8.8
+
+- Support Python 3.14.
+- Rebuild package with oneAPI MKL 2025.2.
 
 2025.1.6
 
@@ -131,7 +136,7 @@ References
 
 from __future__ import annotations
 
-__version__ = '2025.1.6'
+__version__ = '2025.8.8'
 
 __all__ = [
     '__version__',
@@ -259,7 +264,7 @@ def API(dllname=None):
                 return param
             return from_param_(param)
 
-        cls.from_param = classmethod(from_param)  # type: ignore
+        cls.from_param = classmethod(from_param)  # type: ignore[assignment]
         return cls
 
     def outer(a, b, skip=tuple()):
